0a4d0aec8c66c0b8c6996d980569511192623e5d
naoto74 <106652928+naoto74@users.noreply.github.com>
2022-07-16 16:50:49 +0900

    pwaのテスト

---
+++ sw.js
@@ -0,0 +1,30 @@
+const version = "Ver.1.4";
+self.addEventListener("install",e=>{
+    e.waitUntil(
+        caches.open(version).then(cache=>{
+            return cache.addAll([
+                "index.html",
+                "main.css",
+                "manifest.json",
+                "pwa-logo.png",
+                "sw.js"
+            ]);
+        })
+    );
+});
+self.addEventListener("fetch",e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
+});
+self.addEventListener("activate",e=>{  
+    e.waitUntil(
+        caches.keys().then(cache=>{
+            cache.map(name=>{
+                if(version !== name) caches.delete(name);
+            })
+        })
+    );
+});
